3260e4349148e8f1ba29c3abbb477209e7edd227
Peter <48835262+petermagdy91@users.noreply.github.com>
2020-06-26 18:36:14 +0200

    styling

--- webpack.dev.js
+++ webpack.dev.js
@@ -27,7 +27,7 @@ module.exports = {
   },
   plugins: [
     new HtmlWebPackPlugin({
-      template: "./src/views/index.html",
+      template: "./src/client/views/index.html",
       filename: "./index.html"
     }),
     new CleanWebpackPlugin({
